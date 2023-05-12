222d633a00467ef8eac031fd8f344f3af9b217e1
kero <panenko121@gmail.com>
2023-05-12 14:14:09 +0200

    Finish Ps2

--- ps1/task_1.c
+++ ps1/task_1.c
@@ -130,15 +130,160 @@ void counter(const int input_array[], const int array_size, int result_array[2])
 	result_array[1] = odd;
 	
 }
+unsigned long sum_squared(const int line)
+{
+    int forwhile = line;
+    int s = 0;
+    int d = 0;
+    long int w = 0;
+    long int number = 0;
+    int add = 1;
+    if(forwhile < 0)
+    {
+    	return 0;
+    }
+    if(line > s)
+    {
+    	for(int i = 0; i < line; i++)
+    	{
+    		s++;
+    	}
+    }
+    else
+    {
+    return 0;
+    }
+    
+    s = s - line;
+    if (forwhile != 1) 
+    {
+	    for (d = forwhile + 1; s < forwhile; d-- , s++ , number = ((number * d  + 0) / s)) 
+	    {    
+		 w = w + number * number + add;
+	    }
+	    if(w > 0)
+	    {
+	    	return w + 1;
+	    }	    	
+	    else
+	    {
+	    	return 0;
+	    }	         	 
+    }
+    if(forwhile == 1)
+    {
+    	return 2;
+    }
+    else
+    {
+    	return 1;
+    }
+      
+}
+
+int array_min(const int input[], const int array_size)
+{
+	if (input == NULL)
+	{
+ 		return -1 ;
+	}
+	
+	int burmalda = input[0];
+	
+	for(int i = 0; i < array_size; i++)
+	{
+		if(input[i] < burmalda)
+		{
+			burmalda = input[i];
+		}
+					
+		
+	}
+	return burmalda;
+	
+}
+
+
+
+
+int array_max(const int input[], const int array_size)
+{
+	if(input == NULL)
+	{
+		return -1;	
+	}
+	int bururi = input[0];
+	for(int i = 0; i < array_size;i++)
+	{
+		if(input[i] > input[0])
+		{
+			bururi = input[i];
+		}
+		
+	}
+	
+	return bururi;
+}
+
+unsigned long special_counter(const int input_array[], const int array_size)
+{
+	int sum = 0;
+	for(int i = 0; i < array_size; i++)
+	{
+		if(i % 2 == 0)
+		{
+			sum += input_array[i];
+		}
+		else
+		{
+			sum += input_array[i] * input_array[i];
+		}
+	}
+	return sum;
+}
+int special_numbers(const int input_array[], const int array_size, int result_array[])
+{
+	
+	int result = 0;
+	int j = 0;
+	int count = 0;
+		
+		for(j = 0; j < array_size; j++)
+		{
+			result += input_array[j];
+							
+		}
+	
+	
+	for(int h = 0; h < array_size; h++)
+	{
+		int finish = result - input_array[h];
+				
+		if(input_array[h] > finish)
+		{
+			
+			result_array[count] = input_array[h];
+			count++;
+			
+					
+		}
+		result = finish;
+	} 
+	return count;
+}
+
+
 
 
 
 int main(void) {
     
-        int input_array[] = {1,2,3,4,5};
-	int result_array[2];
-	counter(input_array, 5, result_array);
-	printf("%d %d\n", result_array[0], result_array[1]);
+int input_array[] = {16,17,4,3,5,2};
+int result_array[6];
+int count = special_numbers(input_array, 6, result_array);
+for(int i = 0; i < count; i++){
+    printf("%d ", result_array[i]);
+}
 	    
     return 0;
 }
