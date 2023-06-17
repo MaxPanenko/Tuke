28ba7687bcf364ff0d852e1af6f9e85586080c21
kero <panenko121@gmail.com>
2023-06-17 18:16:21 +0200

    Edit ps5

--- ps5/qr.c
+++ ps5/qr.c
@@ -12,7 +12,17 @@ char decode_byte(const bool bits[8]);
 
 int main()
 {	
+	bool bits1[8];
+encode_char('A', bits1);
+for(int i = 0; i < 8; i++){
+    printf("%d", bits1[i]);
+}
+printf("\n");
+// prints: 01000001
 
+bool bits2[8] = {0,1,0,0,0,0,0,1};
+printf("%c\n", decode_byte(bits2));
+// prints: A
 }
 
 void blocks_to_bytes(const int cols, const int offset, bool blocks[offset*8][cols], const int rows, bool bytes[rows][8])
