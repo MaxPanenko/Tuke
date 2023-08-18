f0a57a0aad300518a05777cbf17f90c34cea0edd
kero <panenko121@gmail.com>
2023-08-18 06:55:31 +0200

    I DID IT, FIRST TASK OF THE SECOND SEMESTERgit add .git add .!

--- semester2/playfair.c
+++ semester2/playfair.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "playfair.h"
 
 char* getStartValue(const char* key);
 char* playfair_encrypt(const char* key, const char* text);
+char* changeSifr(const char* text);
 
 int main()
 {
     char *encrypted;
-    encrypted = playfair_encrypt("SECRETKEY", "HELLOWORLD");
+    encrypted = playfair_encrypt("SECRET", "Hello World!");
     printf("%s\n", encrypted);
     free(encrypted);
     return 0;
@@ -18,7 +20,7 @@ int main()
 
 char* playfair_encrypt(const char* key, const char* text)
 {
-    char* startValue = getStartValue(key); // Call getStartValue
+    char* startValue = changeSifr(text); // Call getStartValue
 
     // Process startValue (encrypt or manipulate it)
 
@@ -27,27 +29,79 @@ char* playfair_encrypt(const char* key, const char* text)
 
 char* getStartValue(const char* key)
 {
-    char* result = malloc((strlen(key) + strlen(ALPHA) + 1) * sizeof(char));
+    int key_length = strlen(key);
+    int alpha_length = strlen(ALPHA);
+
+    char* result = malloc((key_length + alpha_length + 1) * sizeof(char));
+
+    if (result == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
     strcpy(result, key);
 
-    char remaining_chars[strlen(ALPHA) + 1];
+    char remaining_chars[alpha_length + 1];
     strcpy(remaining_chars, ALPHA);
 
-    int key_length = strlen(key);
     for (int i = 0; i < key_length; i++)
     {
         for (int j = 0; j < strlen(remaining_chars); j++)
         {
             if (key[i] == remaining_chars[j])
-            {      
-                remaining_chars[j] = remaining_chars[j+1];
-                remaining_chars[j+1] = '\0';
+            {
+                
+                memmove(&remaining_chars[j], &remaining_chars[j + 1], alpha_length - j);
                 break;
             }
         }
     }
 
     strcat(result, remaining_chars); // Append the remaining characters to the result
+
+    return result;
+}
+
+char* changeSifr(const char* text)
+{
+    int result_length = strlen(text) * 2;
+    char* result = (char*)malloc((result_length + 1) * sizeof(char));
+    if (result == NULL)
+    {
+        return NULL;
+    }
+
+    int j = 0;
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if(text[i] == '!'
+            || text[i] == '.'
+            || text[i] == '@'
+            || text[i] == '/'
+            || text[i] == ','
+            || text[i] == '['
+            ||text[i] == ']'
+            || text[i] == '{'
+            || text[i] == '}'
+            )
+        {
+            result[j++] = 'X'; 
+        }
+        else
+        {
+            result[j++] = toupper(text[i]);
+
+            if (toupper(text[i]) == toupper(text[i + 1]))
+            {
+                result[j++] = 'X';        
+            }
+        }
+       
+
+    }
+
+    result[j] = '\0';
 
     return result;
 }
