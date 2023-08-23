d6ab2ce483baccf602161cb6853a9b33e13846c7
kero <panenko121@gmail.com>
2023-08-23 07:02:21 +0200

    Done 2 task and almost the first one

--- semester2/playfair.c
+++ semester2/playfair.c
@@ -7,33 +7,54 @@
 
 char* getStartValue(const char* key);
 char* changeSifr(const char* text);
+char* reverse(const char* reverse);
 
 int main()
 {
-    char *encrypted,*decrypted;
-    encrypted = playfair_encrypt("world", "Hello");
+    char *encrypted,*decrypted, *reversed;
+    encrypted = playfair_encrypt("belfast", "Hello");
     printf("%s\n", encrypted);
-    decrypted = playfair_decrypt("SeCrEt", encrypted);
+    decrypted = playfair_decrypt("belfast", encrypted);
     printf("%s\n", decrypted);
+    reversed = reverse("Hello world!");
+    printf("%s\n", reversed);
     free(encrypted);
     free(decrypted);
+    free(reversed);
     return 0;
 }
 
+char* reverse(const char* text) {
+    if (text == NULL) {
+        return NULL;
+    }
+    
+    int length = strlen(text);
+    char* reversed = (char*)malloc((length + 1) * sizeof(char));
+    
+    int j = 0;
+    for (int i = length - 1; i >= 0; i--) 
+    {
+        reversed[j++] = toupper(text[i]);
+    }
+    reversed[j] = '\0';
+    
+    return reversed;
+}
+
 char* playfair_encrypt(const char* key, const char* text)
+{
+    char *sifr = changeSifr(text);
+    return sifr;
+}
+
+char* playfair_decrypt(const char* key, const char* text)
 {
     char *sifr = changeSifr(text);
   
 
 
     return sifr;
-}
-
-char* playfair_decrypt(const char* key, const char* text)
-{
-    char* startValue = getStartValue(key);
-
-    return startValue;
 }
 
 char* getStartValue(const char* key)
@@ -125,5 +146,51 @@ char* changeSifr(const char* text)
 
     result[j] = '\0';
 
+    /*
+    char array[5][5];
+    int vremya = 0;
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            array[i][j] = result[vremya];
+            vremya++;
+        }
+    }
+
+    int h = 0;
+    int f = 0;
+    int one = 0;
+    int two = 0;
+    char* superMegaResult = (char*)malloc(strlen(result) * sizeof(char));
+   
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if(result[h] == array[i][j])
+            {
+                f = i;
+                h++;
+            }
+         
+            if(result[h + 1] == array[i][j])
+            {
+                one = i;
+                two = j;
+
+                if(one < f)
+                {
+                    superMegaResult[i] = array[one+1][two];
+                }
+            }    
+        }
+        
+         superMegaResult[h] = '\0';
+
+    }
+
+    return superMegaResult;*/
     return result;
 }
